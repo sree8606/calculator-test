0b0e66bcea204ea614c7dee027f55ed6711b05fc
sree8606 <sreelakshmickd3214@gmail.com>
2025-09-24 12:01:02 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendValue(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('result').value = eval(document.getElementById('result').value);
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
